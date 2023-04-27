7bdb11b4911821be55cfa3b4eeca0e36811cd2c1
montanpi <12225722+montanpi@users.noreply.github.com>
2023-04-27 22:42:37 +0900

    add loading and error

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -5,9 +5,18 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
 import { MatSelectModule } from '@angular/material/select'
 import { MatCardModule } from '@angular/material/card'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 
 @NgModule({
   declarations: [],
-  exports: [MatAutocompleteModule, MatInputModule, MatIconModule, MatButtonModule, MatSelectModule, MatCardModule],
+  exports: [
+    MatAutocompleteModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class MaterialModule {}
--- src/app/modules/weather/weather-facade/weather-facade.service.ts
+++ src/app/modules/weather/weather-facade/weather-facade.service.ts
@@ -10,6 +10,8 @@ import { WeatherProvider } from 'src/app/modules/weather/types/weather-provider'
 export class WeatherFacadeService {
   private _currentWeatherProvider: WeatherProvider
   private _weatherData$ = new BehaviorSubject<WeatherData | null>(null)
+  private _loading$ = new BehaviorSubject<boolean>(false)
+  private _error$ = new BehaviorSubject<boolean>(false)
 
   constructor(@Inject(WEATHER_PROVIDER) private readonly _weatherProviders: WeatherProvider[]) {
     this._currentWeatherProvider = this._weatherProviders[0]
@@ -34,9 +36,26 @@ export class WeatherFacadeService {
     }
   }
 
+  get loading$(): Observable<boolean> {
+    return this._loading$
+  }
+
+  get error$(): Observable<boolean> {
+    return this._error$
+  }
+
   async getWeatherByCoordinates(latitude: number, longitude: number): Promise<void> {
-    const wd = await this._currentWeatherProvider.getWeatherData(latitude, longitude)
-    this._weatherData$.next(wd)
+    try {
+      this._weatherData$.next(null)
+      this._error$.next(false)
+      this._loading$.next(true)
+      const wd = await this._currentWeatherProvider.getWeatherData(latitude, longitude)
+      this._weatherData$.next(wd)
+    } catch (error) {
+      this._error$.next(true)
+    } finally {
+      this._loading$.next(false)
+    }
   }
 
   async getWeatherByGeolocation(): Promise<void> {
